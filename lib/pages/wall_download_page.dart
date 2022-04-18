4a29f7e0104c142718b97b5c5ce929973629ad3d
Mattia Frezza <mat.frezza@gmail.com>
2022-04-19 00:25:01 +0200

    better navigation

--- lib/pages/wall_download_page.dart
+++ lib/pages/wall_download_page.dart
@@ -29,43 +29,63 @@ class _WallDownloadPageState extends State<WallDownloadPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Hero(
-        tag: widget.item.id,
-        child: Stack(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height,
-              child: Image(
-                  image: NetworkImage(widget.item.thumbs.original),
-                  fit: BoxFit.cover
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Hero(
+          tag: widget.item.id,
+          child: Stack(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: Image(
+                    image: NetworkImage(widget.item.thumbs.original),
+                    fit: BoxFit.cover
+                ),
               ),
-            ),
-            Center(
-              child: CircularProgressIndicator(
-                color: Colors.white,
+              Center(
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
               ),
-            ),
-            PhotoView(
-              initialScale: PhotoViewComputedScale.covered * 1.0,
-              enableRotation: false,
-              enablePanAlways: false,
-              imageProvider: CachedNetworkImageProvider(widget.item.path),
-            ),
-          ],
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-         // onPressed: () => _saveNetworkImage(context),
-        onPressed: () {
-          showModalBottomSheet<void>(
-            context: context,
-            builder: (_) => DetailsBottomSheet(details: responseDetails.data,),
-          );
-        },
-        child: const Icon(Icons.info),
-        backgroundColor: Colors.blueGrey,
-        foregroundColor: Colors.white,
+              PhotoView(
+                initialScale: PhotoViewComputedScale.covered * 1.0,
+                enableRotation: false,
+                enablePanAlways: false,
+                imageProvider: CachedNetworkImageProvider(widget.item.path),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                padding: EdgeInsets.fromLTRB(0, 50, 20, 20),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(CircleBorder()),
+                    backgroundColor: MaterialStateProperty.all(Colors.blueGrey),
+                    shadowColor: MaterialStateProperty.all(Colors.black)
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(19, 10, 10, 10),
+                    child: Icon(Icons.arrow_back_ios),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              )
+            ],
+          )
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet<void>(
+              context: context,
+              builder: (_) => DetailsBottomSheet(details: responseDetails.data,),
+            );
+          },
+          child: const Icon(Icons.info),
+          backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.white,
+        ),
       ),
     );
   }
